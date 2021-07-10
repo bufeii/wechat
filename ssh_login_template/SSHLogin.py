97d203452e7ec63bb9977b25f0772e08fb5d70b0
UmfintechWtc <31309558+UmfintechWtc@users.noreply.github.com>
2021-07-10 20:28:27 +0800

    Update SSHLogin.py
    port 传参

--- ssh_login_template/SSHLogin.py
+++ ssh_login_template/SSHLogin.py
@@ -4,24 +4,25 @@
 """
     Created on 2021/6/10 11:46
 """
+import paramiko
 
 class SSHLogin():
     """
         创建ssh登录对象
     """
-    def __init__(self,host,user,password):
+    def __init__(self,host,user,port,password):
         self.host = host
         self.user = user
         self.password = password
-        self.login_Port = 10086
+        self.login_Port = port
         self.ssh_conn = paramiko.SSHClient()
         self.ssh_conn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.ssh_conn.connect(hostname=self.host, username=self.user, password=self.password, port=self.login_Port, allow_agent=True)
 
     def ExecuteCmd(self,command):
         """
-        @param command: 需要执行命令 
-        @return: 执行命令返回结果 
+        @param command: 需要执行命令
+        @return: 执行命令返回结果
         """
         stdin, stdout, stderr = self.ssh_conn.exec_command(command)
         cmd_result = stdout.read().decode()
